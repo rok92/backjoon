62d7fceb13772a93c188f7a225ea7f75b6076ac3
Rok <102907482+rok92@users.noreply.github.com>
2022-10-12 03:58:52 +0900

    [Silver IV] Title: 30, Time: 100 ms, Memory: 31620 KB -BaekjoonHub

---
+++ 백준/Silver/10610. 30/30.py
@@ -0,0 +1,13 @@
+N = list(input())
+N.sort(reverse = True)
+sum = 0
+
+if '0' not in N :
+    print(-1)
+else :
+    for i in N :
+        sum += int(i)
+    if(sum % 3 != 0) :
+        print(-1)
+    else :
+        print(''.join(N))
