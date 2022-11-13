12cb30bc105f52272535a8467c964d4c8361693e
Rok <102907482+rok92@users.noreply.github.com>
2022-11-13 23:33:12 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
@@ -1,6 +1,5 @@
 def solution(my_string, n):
-    answer = []
+    answer = ''
     for i in range(len(my_string)) :
-        for j in range(n) :
-            answer.append(my_string[i])
-    return "".join(answer)
+        answer += my_string[i]*n
+    return answer
