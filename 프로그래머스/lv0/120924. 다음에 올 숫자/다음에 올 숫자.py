fb1655fb5e1a7cc21c971d06bb42fc2f3780b898
Rok <102907482+rok92@users.noreply.github.com>
2022-11-14 14:54:50 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -0,0 +1,9 @@
+def solution(common):
+    answer = 0
+    num = common[1] - common[0]
+    if common[1] + num == common[2] :
+        answer = common[len(common)-1] + num
+    else :
+        num = common[1] // common[0]
+        answer = common[len(common) - 1] * num
+    return answer
