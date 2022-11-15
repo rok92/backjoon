f100435083c8451fa692db60fa1fcc7d06cca7b3
Rok <102907482+rok92@users.noreply.github.com>
2022-11-15 20:52:54 +0900

    [level 0] Title: 7의 개수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.py
@@ -0,0 +1,8 @@
+def solution(array):
+    answer = 0
+    stack = []
+    for i in array :
+        for j in range(len(str(i))) :
+            if str(i)[j] == '7' :
+                answer += 1
+    return answer
