a8be315f8db3db4d3fc3efa3f01ffd31a361ba1a
Rok <102907482+rok92@users.noreply.github.com>
2022-11-20 19:30:20 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.17 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+    answer = ''
+    a = list(my_string)
+    for i in a :
+        if i.isupper() :
+            answer += i.lower()
+        else :
+            answer += i.upper()
+    return answer
