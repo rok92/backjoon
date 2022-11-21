df19e3bd762fae4c7344ff48ad500c953076b9cb
Rok <102907482+rok92@users.noreply.github.com>
2022-11-21 17:00:57 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,17 +1,14 @@
+def plus(N) :
+    if N == 1 :
+        return 1
+    elif N == 2 :
+        return 2
+    elif N == 3 :
+        return 4
+    else :
+        return plus(N-1) + plus(N-2) + plus(N-3)
+
 T = int(input())
 for i in range(T) :
     N = int(input())
-    dp = [0] * (N+1)
-    if N == 1 :
-        print(1)
-    elif N == 2 :
-        print(2)
-    elif N == 3 :
-        print(4)
-    else :
-        dp[1] = 1
-        dp[2] = 2
-        dp[3] = 4
-        for j in range(4, N+1) :
-            dp[j] = dp[j-1] + dp[j-2] +dp[j-3]
-        print(dp[N])
+    print(plus(N))
