0c81e657b1d91a1f222171a845e113d8b2142d83
Rok <102907482+rok92@users.noreply.github.com>
2022-11-23 20:05:49 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.01 ms, Memory: 9.96 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -4,7 +4,7 @@ def solution(X, Y):
     for i in array :
         for j in range(min(X.count(i), Y.count(i))) :
             answer.append(i)
-    answer = sorted(answer, reverse = True)
+    answer.sort(reverse = True)
     if len(answer) == 0 : return "-1"
     if answer[0] == "0" : return "0"
     return ''.join(answer)
