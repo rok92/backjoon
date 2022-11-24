57eedde0773e68feeb26d6c35bc7a543bbbf83d9
Rok <102907482+rok92@users.noreply.github.com>
2022-11-24 13:44:04 +0900

    [level 1] Title: 삼총사, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.py
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -1,8 +1,6 @@
 def solution(number):
     answer = 0
-    a = []
-    b = []
-    c = []
+    
     for i in range(len(number)-2) :
         for j in range(i+1, len(number)-1) :
             for k in range(j+1, len(number)) :
