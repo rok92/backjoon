4884a89326fdf76c36c0dc7eccef1c5594d6d8bd
Rok <102907482+rok92@users.noreply.github.com>
2022-12-05 11:41:48 +0900

    [unrated] Title: 점 찍기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/140107. 점 찍기/점 찍기.py
+++ 프로그래머스/unrated/140107. 점 찍기/점 찍기.py
@@ -1,7 +1,9 @@
+import math
+
 def solution(k, d):
     answer = 0
     for i in range(0, d+1, k):
-        a = (d*d - i*i) ** (0.5)
+        a = math.sqrt(d*d - i*i) 
         answer += (a // k+1)
     print(answer)
     return answer
