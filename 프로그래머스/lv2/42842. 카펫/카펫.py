e9cd5f2ffcb2affc0722fcc248854d28e4f617e2
Rok <102907482+rok92@users.noreply.github.com>
2022-12-11 21:01:06 +0900

    [level 2] Title: 카펫, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -0,0 +1,16 @@
+def solution(brown, yellow):
+    answer = []
+    k = []
+    y_row = 0
+    y_col = 0
+    
+    for i in range(1, yellow + 1):
+        if yellow % i == 0:
+            y_row = yellow // i
+            y_col = i
+            if (y_row*2 + y_col*2) + 4 == brown:
+                answer.append(y_row + 2)
+                answer.append(y_col + 2)
+                return answer
+
+    return answer
