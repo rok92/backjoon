9e553faa14a9bbf458f70277a05764e19d5dae27
Rok <102907482+rok92@users.noreply.github.com>
2022-12-12 14:28:41 +0900

    [level 2] Title: 피로도, Time: 0.12 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -0,0 +1,14 @@
+from itertools import permutations
+
+def solution(k, dungeons):
+    answer = 0
+    for i in permutations(dungeons, len(dungeons)):
+        count = 0
+        tmp = k
+        for a, b in i:
+            # print(a, b)
+            if tmp >= a :
+                tmp -= b
+                count += 1
+        answer = max(answer, count)
+    return answer
