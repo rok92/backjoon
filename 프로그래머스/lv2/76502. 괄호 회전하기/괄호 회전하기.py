0fed096a12155c7f8f00aa54d50569fadd6859ab
Rok <102907482+rok92@users.noreply.github.com>
2022-12-15 14:24:37 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -13,7 +13,6 @@ def bracket(s):
             else:
                 stack.append(i)
     return 1 if len(stack) == 0 else 0
-    
 
 def solution(s):
     answer = 0
@@ -22,3 +21,46 @@ def solution(s):
             answer += 1
         s = s[1:] + s[:1]
     return answer
+
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+def solution(s):
+    count = 0
+    for i in range(len(s)):
+        stack = []
+        for j in s:
+            if not stack:
+                stack.append(j)
+                continue
+            if stack[-1] == '[' and j == ']':
+                stack.pop()
+            elif stack[-1] == '{' and j == '}':
+                stack.pop()
+            elif stack[-1] == '(' and j == ')':
+                stack.pop()
+            else:
+                stack.append(j)
+        s = s[1:] + s[0]
+        if not stack:
+            count += 1
+    return count
