3b2d8d3499b0e6f03f315cd68f60213fd7c06d25
Rok <102907482+rok92@users.noreply.github.com>
2022-12-20 18:55:16 +0900

    [level 2] Title: 2 x n 타일링, Time: 13.45 ms, Memory: 11.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -0,0 +1,9 @@
+def solution(n):
+    answer = 0
+    dp = [0 for _ in range(n)]
+    dp[0] = 1
+    dp[1] = 2
+    for i in range(2, n):
+        dp[i] = (dp[i-1]+dp[i-2])%1000000007
+    answer = dp[n-1]
+    return answer
