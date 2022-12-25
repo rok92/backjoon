832b0fd6862bd129747999038c3a60573eaf33ff
Rok <102907482+rok92@users.noreply.github.com>
2022-12-25 14:09:54 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.34 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = 0
+    if n == 1: return 1
+    dp = [0 for _ in range(n+1)]
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n+1):
+        dp[i] = (dp[i-2] + dp[i-1])%1234567
+    answer = dp[-1]
+    return answer
