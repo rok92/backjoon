d567b475f626bba9fadf755ddd96cc8147bae2c2
Rok <102907482+rok92@users.noreply.github.com>
2023-01-19 22:50:41 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.40 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -3,5 +3,4 @@ def solution(numbers):
     for i in range(len(numbers)):
         for j in range(i+1, len(numbers)):
             answer.add(numbers[i] + numbers[j])
-    answer = sorted(list(answer))
-    return answer
+    return sorted(list(answer))
