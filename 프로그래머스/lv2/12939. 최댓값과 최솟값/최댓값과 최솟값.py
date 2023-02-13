0b7ff86528e2ab49ab89b1aa442a9c880836414b
Rok <102907482+rok92@users.noreply.github.com>
2023-02-13 16:00:10 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,5 +1,6 @@
 def solution(s):
     answer = ''
-    arr = list(map(int, s.split()))
-    answer = str(min(arr))+" "+str(max(arr))
+    s_list = list(map(int, s.split()))
+    print(s_list)
+    answer = str(min(s_list)) + " " + str(max(s_list))
     return answer
