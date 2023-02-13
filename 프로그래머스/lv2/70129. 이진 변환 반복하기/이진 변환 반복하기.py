4aad9d8533decde38c5219ba6d9d96f6cbb384bb
Rok <102907482+rok92@users.noreply.github.com>
2023-02-13 19:39:46 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.32 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,16 +1,16 @@
 def solution(s):
     answer = []
     zero_c = 0
-    count = 0
+    count_up = 0
     while s != '1':
         if '0' in s:
             zero_c += s.count('0')
             s = s.replace('0', '')
             
-        length = len(s)
-        s = str(format(length, 'b'))
-        count += 1
-    
-    answer.append(count)
+        s = str(format(len(s), 'b'))
+        count_up += 1
+        
+    answer.append(count_up)
     answer.append(zero_c)
+    
     return answer
