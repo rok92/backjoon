34a12d2165e18f76b67d7ddf3db873c2451415bc
Rok <102907482+rok92@users.noreply.github.com>
2023-02-14 16:32:59 +0900

    [level 2] Title: 숫자의 표현, Time: 5.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,12 +1,12 @@
 def solution(n):
     answer = 0
-    for i in range(1, n+1):
+    
+    for i in range(1, n + 1):
         n_sum = 0
-        for j in range(i, n+1):
+        for j in range(i, n + 1):
             n_sum += j
             if n_sum == n:
                 answer += 1
-                break
             if n_sum > n:
                 break
     return answer
