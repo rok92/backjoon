00bd8bc92e83476dda5745ed08ef54506cddff3d
Rok <102907482+rok92@users.noreply.github.com>
2023-02-14 17:35:04 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,12 +1,10 @@
 def solution(n, words):
     answer = [0, 0]
-    use_word = []
-    use_word.append(words[0])
+    use_word = [words[0]]
     for i in range(1, len(words)):
         if words[i][0] == use_word[-1][-1] and words[i] not in use_word:
             use_word.append(words[i])
         else:
             answer = [(i%n)+1, (i//n)+1]
             break
-        
     return answer
