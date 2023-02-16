6a313ff57056e56479c1d5a67927eefd2c6eb845
Rok <102907482+rok92@users.noreply.github.com>
2023-02-16 17:38:18 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,9 +1,11 @@
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a%b
+    return a
+
+def lcm(a,b):
+    return (a*b / gcd(a,b))
+
 def solution(n, m):
-    a = n
-    b = m
     
-    while m%n:
-        r = m%n
-        m = n
-        n = r
-    return [n, a*b/n]
+    return [gcd(n, m), lcm(n, m)]
