8cdaeb82e6945865b4a48354dd98b55c83b32e99
Rok <102907482+rok92@users.noreply.github.com>
2023-02-16 16:34:14 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
@@ -1,8 +1,7 @@
 def solution(arr):
     answer = []
-    arr.remove(min(arr))
     answer = arr
-    if len(arr) < 2:
+    answer.remove(min(answer))
+    if len(answer) < 2:
         answer.append(-1)
-        
     return answer
