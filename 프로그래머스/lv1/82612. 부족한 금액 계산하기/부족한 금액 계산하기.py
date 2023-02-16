6629f3daa75045b6979b709b8ec0af51e5f0c4a4
Rok <102907482+rok92@users.noreply.github.com>
2023-02-16 17:08:26 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -1,12 +1,12 @@
 def solution(price, money, count):
     answer = 0
-    ttl = 0
-    array_p = []
-    for i in range(1, count+1):
-        money -= price*i
-        if money < 0:
-            answer = abs(money)
-        else:
-            answer = 0
+    price_arr = []
+    for i in range(1, count + 1):
+        price_arr.append(price*i)
+    ttl = sum(price_arr)
+    if ttl - money > 0:
+        answer = ttl - money
+    else:
+        return 0
     
     return answer
