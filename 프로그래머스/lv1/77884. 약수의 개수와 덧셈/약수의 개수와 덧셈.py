9538e8614a0942b7d7e295470ea96e8755635c67
Rok <102907482+rok92@users.noreply.github.com>
2023-02-16 16:53:13 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.80 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,14 +1,14 @@
 def solution(left, right):
-    answer = 0
+    answer = []
     for i in range(left, right+1):
         count_i = 0
         for j in range(1, i+1):
             if i % j == 0:
                 count_i += 1
         if count_i % 2 == 0:
-            answer += i
+            answer.append(i)
         else:
-            answer -= i
+            answer.append(-i)
     print(answer)
-    return answer
+    return sum(answer)
 
