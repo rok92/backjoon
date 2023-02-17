a463aa41e34970168f7828aa9b1893da48d47a2f
Rok <102907482+rok92@users.noreply.github.com>
2023-02-17 15:56:51 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,9 +1,12 @@
-from math import gcd
+def gcd(a, b):
+    if a % b == 0:
+        return b
+    else:
+        return gcd(b, a%b)
 
 def solution(arr):
     arr.sort()
     answer = arr[0]
-    
     for i in arr:
         answer = answer*i // gcd(answer, i)
     return answer
