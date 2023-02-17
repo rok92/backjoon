0bd90a17b038c064ee3cf6e728b0dd1d62d84011
Rok <102907482+rok92@users.noreply.github.com>
2023-02-17 16:53:38 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.py
+++ 프로그래머스/lv1/12901. 2016년/2016년.py
@@ -1,5 +1,5 @@
 def solution(a, b):
     days = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
     month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    answer = sum(month[:a-1]) + b-1
-    return days[answer%7]
+    answer = days[(sum(month[:a-1]) + b-1)%7]
+    return answer
