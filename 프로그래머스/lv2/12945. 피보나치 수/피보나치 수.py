8f487682c9e49461e049a7c211991cc7fd754f08
Rok <102907482+rok92@users.noreply.github.com>
2023-02-17 14:52:31 +0900

    [level 2] Title: 피보나치 수, Time: 32.74 ms, Memory: 13.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -1,8 +1,7 @@
 def solution(n):
-    answer = []
-    answer.append(0)
-    answer.append(1)
+    answer = [0,1]
     for i in range(2, n + 1):
         k = answer[i-1] + answer[i-2]
         answer.append(k%1234567)
+    
     return answer[-1]
