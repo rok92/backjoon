cc6478b7994af13d7fec394f93db3dac33bfe842
Rok <102907482+rok92@users.noreply.github.com>
2023-02-18 13:27:16 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,7 +1,8 @@
 def solution(n):
-    binary = format(n, 'b').count('1')
+    _2jin = format(n, 'b').count('1')
+    print(_2jin)
     while True:
         n += 1
-        if binary == format(n, 'b').count('1'):
+        if _2jin == format(n, 'b').count('1'):
             break
     return n
