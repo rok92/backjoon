7bd4588a9fefc20e856fa3602ceb5e2bce42913b
Rok <102907482+rok92@users.noreply.github.com>
2023-02-18 13:53:10 +0900

    [level 1] Title: 삼총사, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.py
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -1,9 +1,9 @@
 def solution(number):
     answer = 0
-    
-    for i in range(len(number)-2) :
-        for j in range(i+1, len(number)-1) :
-            for k in range(j+1, len(number)) :
-                if number[i] + number[j] + number[k] == 0 :
+    for a in range(len(number)-2):
+        for b in range(a+1, len(number)-1):
+            for c in range(b+1, len(number)):
+                print(number[c])
+                if number[a] + number[b] + number[c] == 0:
                     answer += 1
     return answer
