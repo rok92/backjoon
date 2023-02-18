05a376be7d346067a6e0e5f8fb82e05b75ff9437
Rok <102907482+rok92@users.noreply.github.com>
2023-02-18 15:44:37 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -3,16 +3,15 @@ def solution(progresses, speeds):
     time = 0
     count = 0
     while len(progresses) > 0:
-        if (progresses[0] + time*speeds[0]) >= 100:
+        if progresses[0] + time*speeds[0] >= 100:
             progresses.pop(0)
             speeds.pop(0)
             count += 1
         else:
+            time += 1
             if count > 0:
                 answer.append(count)
                 count = 0
-            time += 1
     answer.append(count)
-            
     return answer
 
