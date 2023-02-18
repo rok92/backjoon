5aca090bf7575b56ce54d8072856f49ab16e600b
Rok <102907482+rok92@users.noreply.github.com>
2023-02-18 13:17:03 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -14,7 +14,6 @@ def solution(maps):
                 nx = x + dx[i]
                 ny = y + dy[i]
                 if nx < 0 or ny < 0 or nx >= len(maps) or ny >= len(maps[0]): continue
-                if maps[nx][ny] == 0: continue
                 if maps[nx][ny] == 1:
                     maps[nx][ny] = maps[x][y] + 1
                     q.append((nx, ny))
