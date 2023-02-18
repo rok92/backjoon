27aac9d0df8ab7c443b969ad009b842d2b33e515
Rok <102907482+rok92@users.noreply.github.com>
2023-02-18 16:27:45 +0900

    [level 1] Title: 모의고사, Time: 8.09 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,24 +1,18 @@
 def solution(answers):
-    answer = []
+    result = []
     a1 = [1,2,3,4,5]
     a2 = [2,1,2,3,2,4,2,5]
     a3 = [3,3,1,1,2,2,4,4,5,5]
-    b1, b2, b3 = 0, 0, 0
-    for i in range(len(answers)):
-        q1 = i % 5
-        q2 = i % 8
-        q3 = i % 10
-        if a1[q1] == answers[i]:
-            b1 += 1
-        if a2[q2] == answers[i]:
-            b2 += 1
-        if a3[q3] == answers[i]:
-            b3 += 1
-    s = max(b1, b2, b3)
-    if s == b1:
-        answer.append(1)
-    if s == b2:
-        answer.append(2)
-    if s == b3:
-        answer.append(3)
-    return answer
+    score = [0, 0, 0]
+    for index, answer in enumerate(answers):
+        print(index, answer)
+        if answer == a1[index%len(a1)]:
+            score[0] += 1
+        if answer == a2[index%len(a2)]:
+            score[1] += 1
+        if answer == a3[index%len(a3)]:
+            score[2] += 1
+    for index, s in enumerate(score):
+        if s == max(score):
+            result.append(index + 1)
+    return result
