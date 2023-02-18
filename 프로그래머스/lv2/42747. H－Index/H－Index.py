bc20453a67f5c81adb56d3217073863d1938ae78
Rok <102907482+rok92@users.noreply.github.com>
2023-02-18 14:54:22 +0900

    [level 2] Title: H-Index, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -2,7 +2,7 @@ def solution(citations):
     answer = 0
     citations.sort()
     for index, citation in enumerate(citations):
-        print(citation, index)
+        print(index, citation)
         if citation >= len(citations)-index:
             answer = len(citations)-index
             return answer
