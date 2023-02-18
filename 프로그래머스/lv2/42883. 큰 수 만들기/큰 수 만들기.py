3ff2e6cc952ce87cad9342107636f2885143bc6f
Rok <102907482+rok92@users.noreply.github.com>
2023-02-19 02:28:59 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,9 +1,15 @@
 def solution(number, k):
     answer = []
     for i in number :
-        while answer and k > 0 and answer[-1] < i :
-            answer.pop()
-            k -= 1
+        if not answer:
+            answer.append(i)
+            continue
+        if k > 0:
+            while answer[-1] < i:
+                answer.pop()
+                k -= 1
+                if not answer or k <= 0: break
         answer.append(i)
-    answer = ''.join(answer[:len(number)-k])
-    return answer
+    answer = answer[:-k] if k > 0 else answer
+    return ''.join(answer)
+    
