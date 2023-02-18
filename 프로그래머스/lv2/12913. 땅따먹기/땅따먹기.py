1813e93c18d103391a1479b547cace43e7bd8a8a
Rok <102907482+rok92@users.noreply.github.com>
2023-02-19 00:55:59 +0900

    [level 2] Title: 땅따먹기, Time: 147.45 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.py
@@ -1,8 +1,8 @@
 def solution(land):
-    for i in range(0, len(land)-1):
-        land[i+1][0] += max(land[i][1], land[i][2], land[i][3])
-        land[i+1][1] += max(land[i][0], land[i][2], land[i][3])
-        land[i+1][2] += max(land[i][0], land[i][1], land[i][3])
-        land[i+1][3] += max(land[i][0], land[i][1], land[i][2])
-    print(max(land[len(land)-1]))
-    return max(land[len(land)-1])
+    for i in range(1, len(land)):
+        land[i][0] += max(land[i-1][1], land[i-1][2], land[i-1][3])
+        land[i][1] += max(land[i-1][0], land[i-1][2], land[i-1][3])
+        land[i][2] += max(land[i-1][0], land[i-1][1], land[i-1][3])
+        land[i][3] += max(land[i-1][0], land[i-1][1], land[i-1][2])  
+    answer = max(land[-1])
+    return answer
