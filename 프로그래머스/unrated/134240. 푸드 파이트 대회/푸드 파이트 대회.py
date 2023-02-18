836777866551a56e26994718dd6d93c2945a3842
Rok <102907482+rok92@users.noreply.github.com>
2023-02-18 15:14:59 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -3,7 +3,7 @@ from itertools import chain
 def solution(food):
     answer = []
     for i in range(1, len(food)) :
-        for _ in range(food[i] // 2) :
+        for _ in range(food[i]//2):
             answer.append(i)
     answer = ''.join(map(str, answer + [0] + answer[::-1]))
     return answer
