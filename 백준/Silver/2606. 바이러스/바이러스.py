1def1b09984832d3b7f49718a31d00837473b3a1
Rok <102907482+rok92@users.noreply.github.com>
2023-03-19 19:19:55 +0900

    [Silver III] Title: 바이러스, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,30 +1,18 @@
-import sys
-input = sys.stdin.readline
-INF = int(1e9)
+com = int(input())
+conn = int(input())
 
-C = int(input()) # 컴퓨터갯수
-L = int(input()) # 연결된 컴퓨터 갯수
+graph = [[] for _ in range(com+1)]
+visited = [False]*(com+1)
 
-graph = [[INF] * (C+1) for _ in range(C+1)]
+for _ in range(conn):
+  a, b = map(int, input().split())
+  graph[a] += [b]
+  graph[b] += [a]
 
-count = 0
-for a in range(1, C+1) :
-    for b in range(1, C+1) :
-        if a == b :
-            graph[a][b] = 0
-for _ in range(L) :
-    a, b = map(int, input().split())
-    graph[a][b] = 1
-    graph[b][a] = 1
-    
-for k in range(1, C+1) :
-    for a in range(1, C+1) :
-        for b in range(1, C+1) :
-            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
-
-for a in range(1, C+1) :
-    if graph[1][a] == INF or graph[1][a] == 0 :
-        continue
-    else :
-        count += 1
-print(count)
+def dfs(v):
+  visited[v] = 1
+  for i in graph[v]:
+    if visited[i] == 0:
+      dfs(i)
+dfs(1)
+print(sum(visited)-1)
