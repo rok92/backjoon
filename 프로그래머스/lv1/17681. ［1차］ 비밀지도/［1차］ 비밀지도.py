cb55e00e19efbf5187290a1a8ccc9b35ca15836a
Rok <102907482+rok92@users.noreply.github.com>
2023-03-22 15:36:17 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -0,0 +1,11 @@
+def solution(n, arr1, arr2):
+    answer = []
+    a1 = []
+    a2 = []
+    print(format(39,'b'))
+    for i in range(n):
+        num = bin(arr1[i]|arr2[i])
+        num = num[2:].zfill(n)
+        num = num.replace('1', '#').replace('0',' ')
+        answer.append(num) 
+    return answer
