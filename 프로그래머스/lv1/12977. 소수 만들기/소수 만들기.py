318cfc2d83edecb6ecbefc0a5dc505337702e4a4
Rok <102907482+rok92@users.noreply.github.com>
2023-03-22 18:43:07 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -1,8 +1,7 @@
-import math
 import itertools
 
 def isPrime(num):
-    for i in range(2,int(math.sqrt(num))+1):
+    for i in range(2,int(num**0.5)+1):
         if num % i == 0:
             return False
     return True
