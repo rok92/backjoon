344b443b6d375995d2e8ccc569c530d88cd3d65a
Rok <102907482+rok92@users.noreply.github.com>
2023-03-24 21:53:30 +0900

    [Silver II] Title: 유기농 배추, Time: 304 ms, Memory: 31652 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,36 +1,33 @@
-from collections import deque
+import sys
+sys.setrecursionlimit(10000)
 
-T = int(input())
+def dfs(x, y):
+  dx = [1, -1, 0, 0]
+  dy = [0, 0, 1, -1]
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-def bfs(x, y) :
-    queue = deque()
-    queue.append((x, y))
+  for i in range(4):
+    nx = x + dx[i]
+    ny = y + dy[i]
+    
+    if (0 <= nx < n) and (0 <= ny < m):
+        if graph[nx][ny] == 1:
+          graph[nx][ny] = -1
+          dfs(nx, ny)
 
-    while queue :
-        x, y = queue.popleft()
-        for i in range(4) :
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or nx >= N or ny < 0 or ny >= M :
-                continue
-            if field[nx][ny] == 1 :
-                field[nx][ny] = 0
-                queue.append((nx, ny))
-    return True
+t = int(input())
 
-for _ in range(T) :
-    M, N, K = map(int, input().split())
-    field = [[0] * M for _ in range(N)]
+for _ in range(t):
+  m, n, k = map(int, input().split())
+  graph = [[0] * m for _ in range(n)]
+  count = 0
+  
+  for _ in range(k):
+    x, y = map(int, input().split())
+    graph[y][x] = 1
 
-    for _ in range(K) :
-        x, y = map(int, input().split())
-        field[y][x] = 1
-    ans = 0
-    for x in range(N) :
-        for y in range(M) :
-            if field[x][y] == 1 :
-                bfs(x, y)
-                ans += 1
-    print(ans)
+  for i in range(n):
+    for j in range(m):
+      if graph[i][j] == 1:
+        dfs(i,j)
+        count += 1
+  print(count)
