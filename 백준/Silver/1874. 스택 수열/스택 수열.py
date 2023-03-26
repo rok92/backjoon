949fc38072bd3b9b825b8bdbfd87c409dfab34f5
Rok <102907482+rok92@users.noreply.github.com>
2023-03-26 19:06:50 +0900

    [Silver II] Title: 스택 수열, Time: 4036 ms, Memory: 32840 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,27 @@
+n = int(input())
+
+count = 1
+stack = []
+pp = []
+result = True
+
+for _ in range(n):
+  num = int(input())
+
+  while count <= num:
+    stack.append(count)
+    count += 1
+    pp.append('+')
+  if stack[-1] == num:
+    stack.pop()
+    pp.append('-')
+  else:
+    result = False
+    break
+
+if result == False:
+  print('NO')
+else:
+  for i in pp:
+    print(i)
+    
