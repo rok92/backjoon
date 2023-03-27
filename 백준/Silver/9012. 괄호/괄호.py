151dfca0439892359507b9da531147560d96d6d2
Rok <102907482+rok92@users.noreply.github.com>
2023-03-27 19:41:04 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,17 +1,21 @@
-T = int(input())
+import sys
+input = sys.stdin.readline
 
+T = int(input())
 for _ in range(T):
   bracket = input()
+
   stack = []
   for i in bracket:
     if i == '(':
       stack.append(i)
     elif i == ')':
-      if stack:
-        stack.pop()
-      else:
+      if len(stack) == 0:
         print('NO')
         break
+      else:
+        stack.pop()
+
   else:
     if len(stack) == 0:
       print('YES')
