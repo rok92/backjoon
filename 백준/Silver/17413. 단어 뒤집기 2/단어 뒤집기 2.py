6a098e2b71086990b92704ebda10df2d5d056e0e
Rok <102907482+rok92@users.noreply.github.com>
2023-03-29 18:32:50 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 76 ms, Memory: 33604 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -1,24 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-s = list(input().rstrip())
-
+s = list(input().strip())
+start = 0
 idx = 0
-left = 0
 while idx < len(s):
-  if s[idx] == '<':
-    idx += 1
-    while s[idx] != '>':
+    if s[idx] == '<':
       idx += 1
-    idx += 1
-  elif s[idx].isalnum():
-    left = idx
-    while idx<len(s) and s[idx].isalnum():
+      while s[idx] != '>':
+        idx += 1
       idx += 1
-    re = s[left:idx]
-    re.reverse()
-    s[left:idx] = re
-  else:
-    idx += 1
-
-print((''.join(s)))
+    elif s[idx].isalnum():
+      start = idx
+      while idx < len(s) and s[idx].isalnum():
+        idx += 1
+      re = s[start:idx]
+      re.reverse()
+      s[start:idx] = re
+    else:
+      idx += 1
+print("".join(s))
