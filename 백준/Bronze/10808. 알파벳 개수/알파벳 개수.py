2479918cfa456d755ec0599cae700e99ba97a49c
Rok <102907482+rok92@users.noreply.github.com>
2023-04-01 21:04:34 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -0,0 +1,9 @@
+import sys
+
+input = sys.stdin.readline
+n = input()
+alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+for i in range(len(alpha)):
+  ans = n.count(alpha[i])
+  print(ans, end=' ')
